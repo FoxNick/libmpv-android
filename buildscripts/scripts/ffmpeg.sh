85f742257f74a59b6000723737611262ed5449e4
FoxNick <354435383@qq.com>
2026-05-04 17:30:57 +0800

    Add files via upload

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -4,12 +4,12 @@
 . ../../include/path.sh
 
 if [ "$1" == "build" ]; then
-	true
+        true
 elif [ "$1" == "clean" ]; then
-	rm -rf _build$ndk_suffix
-	exit 0
+        rm -rf _build$ndk_suffix
+        exit 0
 else
-	exit 255
+        exit 255
 fi
 
 mkdir -p _build$ndk_suffix
@@ -24,23 +24,15 @@ cpuflags=
 [[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mfpu=neon -mcpu=cortex-a8"
 
 ../configure \
-	--target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
-	--arch=${ndk_triple%%-*} --cpu=$cpu --pkg-config=pkg-config --nm=llvm-nm \
-	--extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
-	--enable-{jni,mediacodec,mbedtls,libdav1d,libxml2} --disable-vulkan \
-	--disable-static --enable-shared --enable-{gpl,version3} \
-	--disable-{stripping,doc,programs} \
-	--disable-{muxers,encoders,devices,filters} \
-	--disable-v4l2-m2m
+        --target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
+        --arch=${ndk_triple%%-*} --cpu=$cpu --pkg-config=pkg-config --nm=llvm-nm \
+        --ar=llvm-ar --ranlib=llvm-ranlib --enable-pic --disable-asm \
+        --extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
+        --enable-{jni,mediacodec,mbedtls,libdav1d,libxml2} --disable-vulkan \
+        --enable-static --disable-shared --enable-{gpl,version3} \
+        --disable-{stripping,doc,programs} \
+        --disable-{muxers,encoders,devices,filters} \
+        --disable-v4l2-m2m
 
 make -j$cores
 make DESTDIR="$prefix_dir" install
-
-ln -sf "$prefix_dir"/lib/libswresample.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libpostproc.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libavutil.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libavcodec.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libavformat.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libswscale.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libavfilter.so "$native_dir"
-ln -sf "$prefix_dir"/lib/libavdevice.so "$native_dir"
--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -6,23 +6,22 @@
 build=_build$ndk_suffix
 
 if [ "$1" == "build" ]; then
-	true
+        true
 elif [ "$1" == "clean" ]; then
-	rm -rf $build
-	exit 0
+        rm -rf $build
+        exit 0
 else
-	exit 255
+        exit 255
 fi
 
 unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	--default-library shared \
-	-Diconv=disabled -Dlua=enabled \
-	-Dlibmpv=true -Dcplayer=false \
-	-Dmanpage-build=disabled
+        --default-library static \
+        -Diconv=disabled -Dlua=enabled \
+        -Dlibmpv=true -Dcplayer=false \
+        -Dmanpage-build=disabled \
+        -Dbuild-date=false
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
-
-ln -sf "$prefix_dir"/lib/libmpv.so "$native_dir"
