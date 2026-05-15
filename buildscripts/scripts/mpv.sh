c9860d2399bd45ae203790bdabec7f8746bae238
FoxNick <354435383@qq.com>
2026-05-15 20:09:05 +0800

    Update mpv.sh

--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -14,14 +14,24 @@ else
         exit 255
 fi
 
+# Android provides Vulkan, but no pkgconfig file
+# you can double-check the version in vulkan_core.h (-> VK_HEADER_VERSION)
+mkdir -p "$prefix_dir"/lib/pkgconfig
+cat >"$prefix_dir"/lib/pkgconfig/vulkan.pc <<"END"
+Name: Vulkan
+Description:
+Version: 1.3.275
+Libs: -lvulkan
+Cflags:
+END
+
 unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-        --default-library static \
-        -Diconv=disabled -Dlua=enabled \
-        -Dlibmpv=true -Dcplayer=false \
-        -Dmanpage-build=disabled \
-        -Dbuild-date=false
+	--default-library shared \
+	-Diconv=disabled -Dlua=enabled -Dvulkan=enabled \
+	-Dlibmpv=true -Dcplayer=false \
+	-Dmanpage-build=disabled
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
