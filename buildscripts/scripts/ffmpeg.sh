1b2b0a3db9cd2d32d8f4de42cec1ed166bfc477a
FoxNick <354435383@qq.com>
2026-05-27 16:47:55 +0800

    Update ffmpeg.sh

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -21,15 +21,25 @@ cpu=armv7-a
 [[ "$ndk_triple" == "i686"* ]] && cpu="i686 --disable-asm"
 
 cpuflags=
+asmflags=
 [[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mfpu=neon -mcpu=cortex-a8"
+# Enable neon/asm/inline-asm for ARM architectures (armv7a, arm64)
+if [[ "$ndk_triple" == "arm"* || "$ndk_triple" == "aarch64"* ]]; then
+        asmflags="--enable-neon --enable-asm --enable-inline-asm"
+        # Add -DPIC for proper PIC support in NEON assembly on AArch64
+        cpuflags="$cpuflags -DPIC"
+else
+        asmflags="--disable-neon --disable-asm --disable-inline-asm"
+fi
 
 ../configure \
  --target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
  --arch=${ndk_triple%%-*} --cpu=$cpu --pkg-config=pkg-config --nm=llvm-nm \
- --ar=llvm-ar --ranlib=llvm-ranlib --enable-pic --disable-asm \
+ --ar=llvm-ar --ranlib=llvm-ranlib --enable-pic $asmflags \
  --extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
- --enable-{jni,mediacodec,mbedtls,libdav1d,libxml2} --disable-vulkan \
+ --enable-{jni,mediacodec,mbedtls,libdav1d,libuavs3d,libxml2} --disable-vulkan \
  --enable-static --disable-shared --enable-{gpl,version3} \
+ --enable-decoder=libuavs3d \
  --disable-{stripping,doc,programs} \
  --disable-{muxers,encoders,devices,filters} \
  --disable-v4l2-m2m
